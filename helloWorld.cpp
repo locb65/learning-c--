4fe9030509806889527b579389f65d77b0906399
locb65 <locb65@gmail.com>
2024-01-28 14:49:34 -0500

    made funcs and named vars

---
+++ helloWorld.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// Hello World Function
+
+// int main() {
+//     cout << "Hello World! \n";
+//     cout << "I am learning C++";
+//     return 0;
+// }
+
+// Variables
+
+// Need to declare type followed by var name and value
+
+// int main() {
+//     int myNum = 10;
+//     cout << myNum;
+// } 
+
+// adding variables
+
+// int main() {
+//     int x = 15;
+//     int y = 24;
+//     int sum = x + y;
+//     cout << sum;
+
+// }
+
+// declaring multiple vars
+
+// int main() {
+//     int x = 5, y = 10, z = 25;
+//     cout << x + y + z;
+// }
+
+// or 
+
+// int main() {
+//     int x, y, z;
+//     x = y = z = 50;
+
+//     cout << x + y + z;
+// }
+
