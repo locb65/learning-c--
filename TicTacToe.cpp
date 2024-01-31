8ad935980e1100354a696efa0b40c32476611ec0
locb65 <locb65@Ubuntu.myguest.virtualbox.org>
2024-01-30 20:38:11 -0500

    placed checkwin() outside of int main()

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int main() {
+
 
 char square[10] = {'o','1','2','3','4','5','6','7','8','9'};
 
@@ -50,3 +50,6 @@ int checkwin() {
         return 1;
     }
 }   
+
+
+int main() {}
